e8e4437da8d91e3048cfa314235214aa39871ccb
Cleyson Lima <cleysonph@gmail.com>
2023-05-19 05:30:04 -0300

    Vídeo 09 - Adicionando validação na nossa aplicação

--- src/main/java/br/com/treinaweb/twtodos/controllers/TodoController.java
+++ src/main/java/br/com/treinaweb/twtodos/controllers/TodoController.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import br.com.treinaweb.twtodos.models.Todo;
 import br.com.treinaweb.twtodos.repositories.TodoRepository;
+import jakarta.validation.Valid;
 
 @Controller
 public class TodoController {
@@ -36,7 +38,10 @@ public class TodoController {
     }
 
     @PostMapping("/create")
-    public String create(Todo todo) {
+    public String create(@Valid Todo todo, BindingResult result) {
+        if (result.hasErrors()) {
+            return "todo/form";
+        }
         todoRepository.save(todo);
         return "redirect:/";
     }
@@ -51,7 +56,10 @@ public class TodoController {
     }
 
     @PostMapping("/edit/{id}")
-    public String edit(Todo todo) {
+    public String edit(@Valid Todo todo, BindingResult result) {
+        if (result.hasErrors()) {
+            return "todo/form";
+        }
         todoRepository.save(todo);
         return "redirect:/";
     }
--- src/main/java/br/com/treinaweb/twtodos/models/Todo.java
+++ src/main/java/br/com/treinaweb/twtodos/models/Todo.java
@@ -11,6 +11,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class Todo {
@@ -19,12 +23,16 @@ public class Todo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
+    @Size(min = 3, max = 100)
     @Column(length = 100, nullable = false)
     private String title;
 
     @Column(nullable = false)
     private LocalDateTime createdAt;
 
+    @NotNull
+    @FutureOrPresent
     @Column(nullable = false)
     @DateTimeFormat(iso = ISO.DATE)
     private LocalDate deadline;
