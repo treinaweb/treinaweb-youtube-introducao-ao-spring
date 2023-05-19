c74687413563a2d46799223304bd521beeec6964
Cleyson Lima <cleysonph@gmail.com>
2023-05-18 23:14:37 -0300

    Vídeo 02 - Entendendo a camada MVC e o sistema de rotas do Spring

---
+++ src/main/java/br/com/treinaweb/twtodos/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package br.com.treinaweb.twtodos.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+    
+}
